dc4c62cd53dda62f1d99d1fe74ef788afb5e170d
Ilya Mozerov <ilyailiilia@gmail.com>
2017-03-23 22:06:41 +0300

    Add player into db on start command

--- bot/bot.py
+++ bot/bot.py
@@ -3,16 +3,33 @@ from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
 
+import sqlite3
+
+DATABASE = "../yaFootball.db"
+
 TOKEN = "357076937:AAGMTWhLSqR31XcCvGTkqbx_I3tCaXQ1KVM"
 
 updater = Updater(token=TOKEN)
 dispatcher = updater.dispatcher
 
+def execute(statement):
+    rv = sqlite3.connect(DATABASE)
+    rv.row_factory = sqlite3.Row
+    rv.execute(statement)
+    rv.commit()
+    rv.close()
+
+
 def start(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+    id = update.message.chat_id
+    telegram_handler = update.message.from_user.username
+    execute("INSERT INTO players (id, telegram_handler) values ({}, '{}')".format(id, telegram_handler))
+    bot.sendMessage(chat_id=id, text=str(id) + " with telegram handler: " + telegram_handler + " was added into db!")
 
 def echo(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text=update.message.text)
+    bot.sendMessage(chat_id=player_id, text=player_id + " was added into db!")
+
+
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
