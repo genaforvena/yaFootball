3d3e0c7ec13567b44f754c9d2bec0f6bf1ff2a0c
Ilya Mozerov <ilyailiilia@gmail.com>
2017-03-25 18:22:57 +0300

    Providing match info

--- bot/bot.py
+++ bot/bot.py
@@ -12,9 +12,18 @@ TOKEN = "357076937:AAGMTWhLSqR31XcCvGTkqbx_I3tCaXQ1KVM"
 updater = Updater(token=TOKEN)
 dispatcher = updater.dispatcher
 
+
+
 def get_db():
     rv = sqlite3.connect(DATABASE)
-    rv.row_factory = sqlite3.Row
+
+    def dict_factory(cursor, row):
+        d = {}
+        for idx, col in enumerate(cursor.description):
+            d[col[0]] = row[idx]
+        return d
+
+    rv.row_factory = dict_factory
     return rv
 
 
@@ -25,11 +34,27 @@ def execute(statement):
     rv.close()
 
 
+def execute_for_result(statement):
+    db = get_db()
+    cursor = db.execute(statement)
+    result = cursor.fetchall()
+    db.close()
+    return result
+
+
 def extract_arguments(update):
     return ' '.join(update.message.text.split(' ')[1:])
 
+
 def get_id(update):
     return update.message.chat_id
+
+
+def get_closest_match(bot, update):
+    id = get_id(update)
+    result = execute_for_result('select * from matches order by id desc limit 1')
+    update.message.reply_text("Next match is " + str(result[0]))
+
 
 def set_name(bot, update):
     id = get_id(update)
@@ -47,13 +72,8 @@ def set_ya_handler(bot, update):
 
 def show_player_info(bot, update):
     id = get_id(update)
-    db = get_db()
-    cursor = db.execute("SELECT * FROM players WHERE id = {}".format(id))
-    result = cursor.fetchall()
-    db.close()
-
-    result_info = [dict(x) for x in result][0]
-    update.message.reply_text("Player info: " + str(result_info))
+    result = execute_for_result("SELECT * FROM players WHERE id = {}".format(id))
+    update.message.reply_text("Player info: " + str(result[0]))
 
 
 def start(bot, update):
@@ -73,8 +93,11 @@ def main():
     set_ya_handler_handler = CommandHandler('set_ya_handler', set_ya_handler)
     dispatcher.add_handler(set_ya_handler_handler)
 
-    show_player_info_handler = CommandHandler("info", show_player_info)
+    show_player_info_handler = CommandHandler('info', show_player_info)
     dispatcher.add_handler(show_player_info_handler)
+
+    get_closest_match_handler = CommandHandler('when', get_closest_match)
+    dispatcher.add_handler(get_closest_match_handler)
 
     # echo_handler = MessageHandler(Filters.text, echo)
     # dispatcher.add_handler(echo_handler)
