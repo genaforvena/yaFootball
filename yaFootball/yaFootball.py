991106f993e4f93d3bc77ff7b703738f1ec93741
Ilya Mozerov <ilyailiilia@gmail.com>
2017-04-06 23:26:50 +0300

    Fixed invalid web hook method

--- yaFootball/yaFootball.py
+++ yaFootball/yaFootball.py
@@ -52,7 +52,7 @@ def close_db(error):
 def webhook_handler():
     if request.method == "POST":
         # retrieve the message in JSON and then transform it to Telegram object
-        update = Update.de_json(request.get_json(force=True))
+        update = telegram.Update.de_json(request.get_json(force=True))
         updater.dispatcher.process_update(update)
     return "ok"
 
